689c5b4923c3303dc762ae2e850f48258259b5a2
alfjesus3 <alfjesus3@gmail.com>
2020-07-02 18:28:53 +0200

    Filtering matches with Ransac

--- features_extractor.py
+++ features_extractor.py
@@ -1,23 +1,50 @@
 import cv2
-import matplotlib.pyplot as plt
 import numpy as np
+from skimage.measure import ransac
+from skimage.transform import FundamentalMatrixTransform
 
 class featuresExtractor():
-    def __init__(self):
+    def __init__(self, t):
         self.prevFr = None
         self.orb = cv2.ORB_create()
-        self.bf = cv2.BFMatcher()
+        self.bf = cv2.BFMatcher(cv2.NORM_HAMMING)
+        self.T = t
+
+    def addExtraCoord(self, pts):
+        return np.concatenate([pts, np.ones((pts.shape[0],1))], axis=1)
+
+    def normalize_pts(self, pts):
+        transPt = self.addExtraCoord(pts)
+        return np.dot(np.linalg.inv(self.T), np.transpose(transPt)).T[:, 0:2]
+
+    def denormalize_pt(self, pt):
+        cords = np.dot(self.T, np.array([pt[0], pt[1], 1]))
+        return int(round(cords[0])), int(round(cords[1]))
+
 
     def extracting_feature_matches(self, img):
 
         features = cv2.goodFeaturesToTrack(np.mean(img, axis = 2).astype(np.uint8), 2500, qualityLevel=0.01, minDistance = 3)
         keypts = [cv2.KeyPoint(x=f[0][0], y=f[0][1], _size=20) for f in features]
         keypts, descrip = self.orb.compute(img, keypts)
-
-        matches = None
+        res = []
+        
         if self.prevFr is not None:
-            matches = self.bf.match(descrip, self.prevFr['descrip'])
+            matches = self.bf.knnMatch(descrip, self.prevFr['descrip'], k=2)
+            for m, n in matches:
+                if m.distance < 0.75 * n.distance:
+                    keypts1 = keypts[m.queryIdx].pt
+                    keypts2 = self.prevFr['keypts'][m.trainIdx].pt
+                    res.append((keypts1, keypts2))
+
+        if len(res) > 0:
+            res = np.array(res)
+            res[:, 0, :] = self.normalize_pts(res[:, 0, :])
+            res[:, 1, :] = self.normalize_pts(res[:, 1, :])
+
+            model, inliers = ransac((res[:, 0], res[:, 1]), FundamentalMatrixTransform, min_samples = 8, residual_threshold=.1, max_trials = 100)
+            res = res[inliers]
 
         self.prevFr = {'keypts': keypts, 'descrip': descrip}
 
-        return keypts, descrip, matches
+        return res #keypts, descrip, matches
--- testing.py
+++ testing.py
@@ -2,16 +2,29 @@
 from features_extractor import featuresExtractor
 from display import displayVideo
 import cv2
+import math
+import numpy as np
 
 
 if __name__ == "__main__":
     cap = cv2.VideoCapture('./files/drivingCar.mp4')
+
+    W = 1920 // 2
+    H = 1080 // 2
+
     dp = displayVideo()
-    fe = featuresExtractor()
+
+    d=1
+    cons = math.sqrt(2)/d
+    T = np.array([[cons, 0, -W*cons], [0, cons, -H*cons], [0, 0, 1]])
+    print(T.shape)
+
+    fe = featuresExtractor(T)
 
     while cap.isOpened():
         ret, frame = cap.read()
         if ret == True:
+            """
             keypts, _, matches = fe.extracting_feature_matches(frame)
             if matches is not None:
                 print(len(matches))
@@ -19,6 +32,16 @@ if __name__ == "__main__":
             for p in keypts:
                 u, v = map(lambda x: int(round(x)), p.pt)
                 cv2.circle(frame, (u,v), color = (40, 255, 0), radius = 3)
+            """
+            matches = fe.extracting_feature_matches(frame)
+            if matches is not None:
+                for pt1, pt2 in matches:
+                    u1, v1 = map(lambda x: int(round(x)), pt1)
+                    u2, v2 = map(lambda x: int(round(x)), pt2)
+                    u1, v1 = fe.denormalize_pt((u1,v1))
+                    u2, v2 = fe.denormalize_pt((u2,v2))
+                    cv2.circle(frame, (u1,v1), color = (40, 255, 0), radius = 3)
+                    cv2.line(frame, (u1, v1), (u2, v2), color=(255,0,0))                    
             dp.process_frame(frame)
         else:
             break    
