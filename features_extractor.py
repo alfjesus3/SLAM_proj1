b505ea59ee9ff9a5cfbbc6f8f549a73489643086
Andre Jesus <alfjesus3@gmail.com>
2020-07-11 15:39:28 +0200

    Essential Matrix pose estimation

--- features_extractor.py
+++ features_extractor.py
@@ -1,7 +1,21 @@
 import cv2
 import numpy as np
 from skimage.measure import ransac
-from skimage.transform import FundamentalMatrixTransform
+from skimage.transform import EssentialMatrixTransform
+
+
+def addExtraCoord(pts):
+    return np.concatenate([pts, np.ones((pts.shape[0],1))], axis=1)
+
+def normalize_pts(pts, T):
+    transPt = addExtraCoord(pts)
+    return np.dot(np.linalg.inv(T), np.transpose(transPt)).T[:, 0:2]
+
+def denormalize_pt(pt, T):
+    cords = np.dot(T, np.array([pt[0], pt[1], 1]))
+    cords = cords/ cords[2]
+    return int(round(cords[0])), int(round(cords[1]))
+
 
 class featuresExtractor():
     def __init__(self, t):
@@ -10,21 +24,10 @@ class featuresExtractor():
         self.bf = cv2.BFMatcher(cv2.NORM_HAMMING)
         self.T = t
 
-    def addExtraCoord(self, pts):
-        return np.concatenate([pts, np.ones((pts.shape[0],1))], axis=1)
-
-    def normalize_pts(self, pts):
-        transPt = self.addExtraCoord(pts)
-        return np.dot(np.linalg.inv(self.T), np.transpose(transPt)).T[:, 0:2]
-
-    def denormalize_pt(self, pt):
-        cords = np.dot(self.T, np.array([pt[0], pt[1], 1]))
-        return int(round(cords[0])), int(round(cords[1]))
-
 
     def extracting_feature_matches(self, img):
-
-        features = cv2.goodFeaturesToTrack(np.mean(img, axis = 2).astype(np.uint8), 2500, qualityLevel=0.01, minDistance = 3)
+        features = cv2.goodFeaturesToTrack(np.mean(img, axis = 2).astype(np.uint8),
+                                        2500, qualityLevel=0.01, minDistance = 3)
         keypts = [cv2.KeyPoint(x=f[0][0], y=f[0][1], _size=20) for f in features]
         keypts, descrip = self.orb.compute(img, keypts)
         res = []
@@ -36,15 +39,45 @@ class featuresExtractor():
                     keypts1 = keypts[m.queryIdx].pt
                     keypts2 = self.prevFr['keypts'][m.trainIdx].pt
                     res.append((keypts1, keypts2))
-
+        
+        Rt = None
+        print(len(res))
         if len(res) > 0:
             res = np.array(res)
-            res[:, 0, :] = self.normalize_pts(res[:, 0, :])
-            res[:, 1, :] = self.normalize_pts(res[:, 1, :])
+            res[:, 0, :] = normalize_pts(res[:, 0, :], self.T)
+            res[:, 1, :] = normalize_pts(res[:, 1, :], self.T)
 
-            model, inliers = ransac((res[:, 0], res[:, 1]), FundamentalMatrixTransform, min_samples = 8, residual_threshold=.1, max_trials = 100)
+            model, inliers = ransac((res[:, 0], res[:, 1]),
+                                    EssentialMatrixTransform, 
+                                    min_samples = 8, 
+                                    residual_threshold=.1, 
+                                    max_trials = 100)
             res = res[inliers]
 
-        self.prevFr = {'keypts': keypts, 'descrip': descrip}
+            Rt = self.extract_Rot_trans(model.params)
 
-        return res #keypts, descrip, matches
+        self.prevFr = {'keypts': keypts, 'descrip': descrip}
+        
+
+        return res, Rt
+    
+    def extract_Rot_trans(self, essen):
+        W = np.mat([[0,-1,0], [1,0,0], [0,0,1]], dtype = float)
+        U, d, Vt = np.linalg.svd(essen)
+
+        assert np.linalg.det(U) > 0
+        if np.linalg.det(Vt) < 0:
+            Vt *= -1.0
+        R = np.dot(np.dot(U,W), Vt) # rot 1
+
+        if np.sum(R.diagonal()) < 0:
+           R = np.dot(np.dot(U,W.T), Vt) # rot 2
+        t = U[:, 2]
+
+        Rt = np.eye(4)
+        Rt[:3,:3] = R
+        Rt[:3, 3] = t
+        
+        return Rt
+        
+
--- testing.py
+++ testing.py
@@ -1,45 +1,40 @@
 #!./usr/bin/env python3
-from features_extractor import featuresExtractor
+from features_extractor import featuresExtractor, denormalize_pt
 from display import displayVideo
 import cv2
 import math
 import numpy as np
 
+import build.g2o
 
 if __name__ == "__main__":
-    cap = cv2.VideoCapture('./files/drivingCar.mp4')
+    cap = cv2.VideoCapture('./files/video.mp4')
+
+    dp = displayVideo()
 
     W = 1920 // 2
     H = 1080 // 2
 
-    dp = displayVideo()
-
     d=1
     cons = math.sqrt(2)/d
     T = np.array([[cons, 0, -W*cons], [0, cons, -H*cons], [0, 0, 1]])
-    print(T.shape)
 
     fe = featuresExtractor(T)
 
     while cap.isOpened():
         ret, frame = cap.read()
         if ret == True:
-            """
-            keypts, _, matches = fe.extracting_feature_matches(frame)
-            if matches is not None:
-                print(len(matches))
+            matches, pose = fe.extracting_feature_matches(frame)
+            
+            if pose is None: 
+                continue
 
-            for p in keypts:
-                u, v = map(lambda x: int(round(x)), p.pt)
-                cv2.circle(frame, (u,v), color = (40, 255, 0), radius = 3)
-            """
-            matches = fe.extracting_feature_matches(frame)
             if matches is not None:
                 for pt1, pt2 in matches:
                     u1, v1 = map(lambda x: int(round(x)), pt1)
                     u2, v2 = map(lambda x: int(round(x)), pt2)
-                    u1, v1 = fe.denormalize_pt((u1,v1))
-                    u2, v2 = fe.denormalize_pt((u2,v2))
+                    u1, v1 = denormalize_pt((u1,v1), T)
+                    u2, v2 = denormalize_pt((u2,v2), T)
                     cv2.circle(frame, (u1,v1), color = (40, 255, 0), radius = 3)
                     cv2.line(frame, (u1, v1), (u2, v2), color=(255,0,0))                    
             dp.process_frame(frame)
