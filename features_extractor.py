0e8d5a8c7b9cdbf2ee41297be5c51da9fbfc4eb8
Andre Jesus <alfjesus3@gmail.com>
2020-07-11 19:46:34 +0200

    Matching between Frames

--- features_extractor.py
+++ features_extractor.py
@@ -16,68 +16,97 @@ def denormalize_pt(pt, T):
     cords = cords/ cords[2]
     return int(round(cords[0])), int(round(cords[1]))
 
+orb = cv2.ORB_create() 
+bf = cv2.BFMatcher(cv2.NORM_HAMMING) 
 
-class featuresExtractor():
-    def __init__(self, t):
-        self.prevFr = None
-        self.orb = cv2.ORB_create()
-        self.bf = cv2.BFMatcher(cv2.NORM_HAMMING)
-        self.T = t
+def extract_features_frame(img):
+    features = cv2.goodFeaturesToTrack(np.mean(img, axis = 2).astype(np.uint8), 
+                                        2500,
+                                        qualityLevel=0.01, 
+                                        minDistance = 3)
+    keypts = [cv2.KeyPoint(x=f[0][0], y=f[0][1], _size=20) for f in features]
+    keypts, descrip = orb.compute(img, keypts)
+    return np.array([(kpt.pt[0], kpt.pt[1]) for kpt in keypts]), descrip
 
+def match_frames(img1, img2):
+    res = []
+    idx1, idx2 = [], [] 
+    # Lowe's ratio test
+    matches = bf.knnMatch(img1.des, img2.des, k=2)
+    for m, n in matches:
+        if m.distance < 0.75 * n.distance:
 
-    def extracting_feature_matches(self, img):
-        features = cv2.goodFeaturesToTrack(np.mean(img, axis = 2).astype(np.uint8),
-                                        2500, qualityLevel=0.01, minDistance = 3)
-        keypts = [cv2.KeyPoint(x=f[0][0], y=f[0][1], _size=20) for f in features]
-        keypts, descrip = self.orb.compute(img, keypts)
-        res = []
-        
-        if self.prevFr is not None:
-            matches = self.bf.knnMatch(descrip, self.prevFr['descrip'], k=2)
-            for m, n in matches:
-                if m.distance < 0.75 * n.distance:
-                    keypts1 = keypts[m.queryIdx].pt
-                    keypts2 = self.prevFr['keypts'][m.trainIdx].pt
-                    res.append((keypts1, keypts2))
-        
-        Rt = None
-        print(len(res))
-        if len(res) > 0:
-            res = np.array(res)
-            res[:, 0, :] = normalize_pts(res[:, 0, :], self.T)
-            res[:, 1, :] = normalize_pts(res[:, 1, :], self.T)
+            idx1.append(m.queryIdx)
+            idx2.append(m.trainIdx)
 
-            model, inliers = ransac((res[:, 0], res[:, 1]),
-                                    EssentialMatrixTransform, 
-                                    min_samples = 8, 
-                                    residual_threshold=.1, 
-                                    max_trials = 100)
-            res = res[inliers]
-
-            Rt = self.extract_Rot_trans(model.params)
-
-        self.prevFr = {'keypts': keypts, 'descrip': descrip}
-        
-
-        return res, Rt
+            keypts1 = img1.pts[m.queryIdx]
+            keypts2 = img2.pts[m.trainIdx]
+            res.append((keypts1, keypts2))
     
-    def extract_Rot_trans(self, essen):
-        W = np.mat([[0,-1,0], [1,0,0], [0,0,1]], dtype = float)
-        U, d, Vt = np.linalg.svd(essen)
+    res = np.array(res)
+    idx1 = np.array(idx1)
+    idx2 = np.array(idx2)
 
-        assert np.linalg.det(U) > 0
-        if np.linalg.det(Vt) < 0:
-            Vt *= -1.0
-        R = np.dot(np.dot(U,W), Vt) # rot 1
+    Rt = None
+    print(len(res))
+    res[:, 0, :] = normalize_pts(res[:, 0, :], img1.T)
+    res[:, 1, :] = normalize_pts(res[:, 1, :], img1.T)
 
-        if np.sum(R.diagonal()) < 0:
-           R = np.dot(np.dot(U,W.T), Vt) # rot 2
-        t = U[:, 2]
+    model, inliers = ransac((res[:, 0], res[:, 1]),
+                            EssentialMatrixTransform, 
+                            min_samples = 8, 
+                            residual_threshold=.15, 
+                            max_trials = 100)
 
-        Rt = np.eye(4)
-        Rt[:3,:3] = R
-        Rt[:3, 3] = t
+    res = res[inliers]
+    Rt = extract_Rot_trans(model.params)
+
+    print(len(res))
+    print(">>>>>>>>>>")
+
+    return idx1[inliers], idx2[inliers], Rt
+
+
+def extract_Rot_trans(essen):
+    W = np.mat([[0,-1,0], [1,0,0], [0,0,1]], dtype = float)
+    U, d, Vt = np.linalg.svd(essen)
+
+    assert np.linalg.det(U) > 0
+    if np.linalg.det(Vt) < 0:
+        Vt *= -1.0
+    R = np.dot(np.dot(U,W), Vt) # rot 1
+
+    if np.sum(R.diagonal()) < 0:
+       R = np.dot(np.dot(U,W.T), Vt) # rot 2
+    t = U[:, 2]
+
+    Rt = np.eye(4)
+    Rt[:3,:3] = R
+    Rt[:3, 3] = t
+    
+    return Rt
+
+
+class Frame(object):
+    def __init__(self, img, T):
+        self.T = T
+        self.Tinv = np.linalg.inv(self.T)
+        self.img = img
+        self.pose = np.eye(4)
         
-        return Rt
-        
+        pts, self.des = extract_features_frame(img)
+        self.pts = normalize_pts(pts, self.T)
+
+
+
+class Point3d(object):
+    # It represents a 3d point obtain through the triangulation procedure
+    def __init__(self, loc):
+        self.location = loc
+        self.frames = []
+        self.idxs = []
+
+    def add_frame_observation(self, frame, idx):
+        self.frames.append(frame)
+        self.idxs.append(idx)
 
--- testing.py
+++ testing.py
@@ -1,11 +1,54 @@
 #!./usr/bin/env python3
-from features_extractor import featuresExtractor, denormalize_pt
+from features_extractor import match_frames, denormalize_pt, Frame, Point3d
 from display import displayVideo
 import cv2
 import math
 import numpy as np
 
 import build.g2o
+
+
+def triangulate_projections(m1, m2, pt1, pt2):
+    return cv2.triangulatePoints(m1[:3], m2[:3], pt1.T, pt2.T).T
+
+frames = []
+def process_frame(frame):
+    frames.append(frame)
+
+    if len(frames) < 2:
+        return 
+
+    idx1, idx2, Rt = match_frames(frames[-1], frames[-2]) 
+
+    frames[-1].pose = np.dot(Rt, frames[-2].pose)
+            
+    # Converting to 3D point
+    pts4d = triangulate_projections(frames[-1].pose, 
+                                    frames[-2].pose, 
+                                    frames[-1].pts[idx1], 
+                                    frames[-2].pts[idx2])
+    pts4d /= pts4d[:, 3:]
+            
+    # rejecting points behind the camera
+    good_pts4 = (pts4d[:, 2] > 0)
+
+    for i, p in enumerate(pts4d):
+        if not good_pts4[i]:
+            continue
+        pt = Point3d(p)
+        pt.add_frame_observation(frames[-1], idx1[i])
+        pt.add_frame_observation(frames[-2], idx2[i])
+
+    for pt1, pt2 in zip(frames[-1].pts[idx1], frames[-2].pts[idx2]):
+        u1, v1 = map(lambda x: int(round(x)), pt1)
+        u2, v2 = map(lambda x: int(round(x)), pt2)
+        u1, v1 = denormalize_pt((u1,v1), T)
+        u2, v2 = denormalize_pt((u2,v2), T)
+        cv2.circle(frame.img, (u1,v1), color = (40, 255, 0), radius = 3)
+        cv2.line(frame.img, (u1, v1), (u2, v2), color=(255,0,0))                    
+
+    dp.process_frame(frame.img)
+
 
 if __name__ == "__main__":
     cap = cv2.VideoCapture('./files/video.mp4')
@@ -19,24 +62,11 @@ if __name__ == "__main__":
     cons = math.sqrt(2)/d
     T = np.array([[cons, 0, -W*cons], [0, cons, -H*cons], [0, 0, 1]])
 
-    fe = featuresExtractor(T)
-
     while cap.isOpened():
         ret, frame = cap.read()
         if ret == True:
-            matches, pose = fe.extracting_feature_matches(frame)
-            
-            if pose is None: 
-                continue
-
-            if matches is not None:
-                for pt1, pt2 in matches:
-                    u1, v1 = map(lambda x: int(round(x)), pt1)
-                    u2, v2 = map(lambda x: int(round(x)), pt2)
-                    u1, v1 = denormalize_pt((u1,v1), T)
-                    u2, v2 = denormalize_pt((u2,v2), T)
-                    cv2.circle(frame, (u1,v1), color = (40, 255, 0), radius = 3)
-                    cv2.line(frame, (u1, v1), (u2, v2), color=(255,0,0))                    
-            dp.process_frame(frame)
+            img = cv2.resize(frame, (W,H))
+            frame = Frame(img, T)
+            process_frame(frame)
         else:
             break    
